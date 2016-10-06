3454f0150bb689df3b1986448019891d712f6661
iliass <iliass24@gmail.com>
2016-10-06 14:26:30 -0400

    added styling changes from Michael and Lisa

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ import React from 'react';
 var Footer = React.createClass({
   render(){
     return (
-        <div className="footer" id="contact">
-            <p>“Java is to JavaScript what Car is to Carpet.” - Chris Heilmann</p> <br></br>
+        <div className="footer">
+            <p className="footerquote">“Java is to JavaScript what Car is to Carpet.” - Chris Heilmann</p> <br></br>
             <p>Copyright smilE-commerce 2016</p>
         </div>
       )
